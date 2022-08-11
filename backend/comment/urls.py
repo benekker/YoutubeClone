251a52e29128f8b68b028c99b435cb6d92aef3c0
Fuller1212 <fullermorrison@gmail.com>
2022-08-11 15:02:33 -0500

    Created POST and PUT functionality in Comment app views.py file

--- backend/comment/urls.py
+++ backend/comment/urls.py
@@ -1,7 +1,8 @@
-import imp
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:video_id>', views.get_comments_by_video_id)
+    path('<str:video_id>', views.get_comments_by_video_id),
+    path('<int:pk>/', views. user_comment)
 ]
--- backend/comment/views.py
+++ backend/comment/views.py
@@ -2,8 +2,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import CommentSerializer
 from .models import Comment
-from rest_framework import Status
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -16,10 +17,17 @@ def get_comments_by_video_id(request, video_id):
         serializer = CommentSerializer(comment, many=True)
         return Response (serializer.data)
 
-@api_view(['GET', 'POST','PUT'])
+@api_view(['POST','PUT'])
 @permission_classes([IsAuthenticated])
-def user_comment (request):
+def user_comment (request, pk):
     if request.method == 'POST':
         serializer = CommentSerializer(data = request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'PUT':
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response (serializer.data, status=status.HTTP_200_OK)
